54adae23b0d0f8244ded2e56f2f1f7339228e861
Alicia <alicia.rannou23@gmail.com>
2018-10-21 23:00:16 +0200

    Prod path for api call

--- www/js/functions.js
+++ www/js/functions.js
@@ -1,4 +1,4 @@
-var api_path = 'https://preprod.raidy.sixteam.tech/';
+var api_path = 'https://raidy.sixteam.tech/';
 
 function apiCall(method, url, jsonData=null, callback=null) {
 	if (callback==null) {
